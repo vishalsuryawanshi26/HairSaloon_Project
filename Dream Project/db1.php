df960cc5ddecdf0bcacafbe6e027a657235a888d
vishalsuryawanshi26 <114423136+vishalsuryawanshi26@users.noreply.github.com>
2023-01-13 16:01:47 +0530

    Add files via upload
    Connect DB and Upload

---
+++ Dream Project/db1.php
@@ -0,0 +1,31 @@
+<?php
+
+//fetching values of form into php variables
+  $fname = $_POST['fname'];
+  $gender = $_POST['gender'];
+  $address = $_POST['address'];
+  $nearby = $_POST['nearby'];
+  $date = $_POST['date'];
+  $time = $_POST['time'];
+
+//   code for database connection
+ $conn = new mysqli('localhost','root','','genform',3306);
+ 
+  if ($conn->connect_error) {
+    die('Connection failed...'.$conn->connect_error);
+  }
+  else {
+
+    $stmt = $conn->prepare( "insert into general_table
+    (fname,gender,address,nearby,date,time)
+    values(?,?,?,?,?,?)
+    ");
+
+    $stmt->bind_param("ssssss",$fname,$gender,$address,$nearby,$date,$time);
+    $stmt->execute();
+    echo "Form successfully submitted 👍. <br>";
+    $stmt->close();
+    $conn->close();
+  }
+
+?>
